e98b807c3e0bfc42024d8d965e113937df2f7165
Nielsen, Clinton <Clinton_Nielsen@Intuit.com>
2017-12-14 16:35:23 +0100

    More songs.

--- resources/data/data.js
+++ resources/data/data.js
@@ -13,13 +13,18 @@ var treeStore = Ext.create('Ext.data.TreeStore', {
         items: [
         	{ text: 'A-M', items: [
         		{ text: 'Angels We Have Heard On High', leaf: true },
+        		{ text: 'Away in a Manger', leaf: true },
         		{ text: 'Carol of the Bells', leaf: true },
         		{ text: 'Frosty the Snowman', leaf: true },
+        		{ text: 'Joy to the world', leaf: true },
         		]
         	},
         	{ text: 'N-Z', items: [
         		{ text: 'O Come O Come Emmanuel', leaf: true },
+        		{ text: 'Oh, Come, All Ye Faithful', leaf: true },
+        		{ text: 'Once in Royal David’s City', leaf: true },
         		{ text: 'Rudolph the Red Nosed Reindeer', leaf: true },
+        		{ text: 'Silent Night', leaf: true },
         		{ text: 'We Wish You a Merry Christmas', leaf: true },
         		]
         	} ]
