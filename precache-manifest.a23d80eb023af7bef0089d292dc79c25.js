d67c45176d29544e65c48277a7f66f976d95434f
Nielsen, Clinton <clinton_nielsen@intuit.com>
2020-02-12 10:54:02 -0700

    Updates

---
+++ precache-manifest.a23d80eb023af7bef0089d292dc79c25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2866ea512c77abfc599a7120aebc97a0",
+    "url": "/christmas-lyrics/index.html"
+  },
+  {
+    "revision": "9d8b474d88c7a535a9a6",
+    "url": "/christmas-lyrics/static/js/2.a5c5d7d5.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/christmas-lyrics/static/js/2.a5c5d7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3768df029224bed66da5",
+    "url": "/christmas-lyrics/static/js/main.d312a29e.chunk.js"
+  },
+  {
+    "revision": "8c7eea3c8465dc09acd1",
+    "url": "/christmas-lyrics/static/js/runtime-main.52603e7e.js"
+  }
+]);
