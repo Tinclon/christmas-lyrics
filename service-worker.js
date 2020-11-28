3d7e8ba73a410ef35c59635d1525112e53e3e709
Nielsen, Clinton <clinton_nielsen@intuit.com>
2020-11-27 20:00:38 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/christmas-lyrics/precache-manifest.d175fab3a20609a4113e293e698efb12.js"
+  "/christmas-lyrics/precache-manifest.c1552f33e737af9f5c69a8008dec75a8.js"
 );
 
 self.addEventListener('message', (event) => {
