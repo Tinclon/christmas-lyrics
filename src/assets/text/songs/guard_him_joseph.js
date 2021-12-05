b088282cf50c7b8ca4b768e99f96aafb6790d76c
Tinclon <clinton.h.nielsen@gmail.com>
2021-12-05 08:53:16 -0700

    Update guard_him_joseph.js

--- src/assets/text/songs/guard_him_joseph.js
+++ src/assets/text/songs/guard_him_joseph.js
@@ -1,1 +1,36 @@
 
+export default () => `
+1. Guard him, Joseph, as he slumbers,
+there behind the stable doors,
+Let no errant sound distress him,
+Tread thou softly on the floor
+
+Linger watchful round the manger,
+Hush thy voice and still the beasts,
+For thou hast within thy keeping
+Christ, the infant Prince of Peace.
+
+2. Guard him, Joseph, through the darkness,
+Watching o'er him till the dawn,
+Bid the shepherds enter meekly;
+stand in wonder; gaze in awe.
+
+Trim thy lamp and shield they candle,
+See they burn all through the night,
+For thou hast within thy keeping
+Christ, the everlasting Light.
+
+3. Guard him, Joseph, from all danger,
+Keep thy vigil nigh his bed,
+Let no harm nor grief befall him,
+Stand thou in a father's stead,
+
+When his mother's arms be weary,
+Hold him gently as thine own,
+For thou hast within thy keeping
+God's own well beloved Son
+
+---
+
+God's own well beloved Son.
+`
