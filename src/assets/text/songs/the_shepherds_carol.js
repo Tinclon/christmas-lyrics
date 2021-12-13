f4591b83b299089bf756a6d487bb741d04deacb3
Tinclon <clinton.h.nielsen@gmail.com>
2021-12-13 09:56:54 -0700

    Create the_shepherds_carol.js

---
+++ src/assets/text/songs/the_shepherds_carol.js
@@ -0,0 +1,11 @@
+export default () => `
+Mary, Mary, hush, see the Child.
+Joseph, Joseph, look, see how mild!
+This is Jesus; this is our King.
+This is our Savior; his praises we sing.
+
+All God's children, come to adore,
+Bringing gifts of love evermore.
+This is Jesus; this is our King.
+This is our Savior; his praises we sing.
+`
