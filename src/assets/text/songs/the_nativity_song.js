0ff670d9833a5b170e9e839f16ee2913cfaac1f6
Tinclon <clinton.h.nielsen@gmail.com>
2021-12-13 10:00:24 -0700

    Create the_nativity_song.js

---
+++ src/assets/text/songs/the_nativity_song.js
@@ -0,0 +1,26 @@
+export default () => `
+1. This is the season beloved of the year.
+Sing a rhyme; Christmastime soon will be here.
+Tell the true story of Jesus' birth,
+When, as a baby, he came to the earth.
+
+2. This is the new star, shining so bright,
+Lighting the world on that first Christmas night.
+This is the angel proclaiming the birth,
+Singing "Hosanna!" and "Peac on the earth!"
+
+3. This is the stable, shelter so bare;
+Cattle and oxen first welcomed him there.
+This is the manger, sweet hay for a bed,
+Waiting for Jesus to cradle his head.
+
+4. These are the shepherds, humble and mild,
+hast'ning to worship the heavenly child.
+These are the wise men who followed the star,
+Frankincense, gold, and myrrh brought from afar.
+
+5. See the sweet other, Mary so fair,
+Joseph, who guided the donkey with care.
+See the dear baby of Bethlehem,
+Little Lord Jesus, the Savior of men.
+`
