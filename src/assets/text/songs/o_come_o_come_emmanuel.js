4601d3dbdbd33089f81938711b8d019a5e6ea039
Tinclon <clinton.h.nielsen@gmail.com>
2021-12-22 22:37:01 -0700

    Update o_come_o_come_emmanuel.js

--- src/assets/text/songs/o_come_o_come_emmanuel.js
+++ src/assets/text/songs/o_come_o_come_emmanuel.js
@@ -11,7 +11,7 @@ Thine own from Satan’s tyranny
 From depths of Hell Thy people save
 And give them victory o’er the grave
 Rejoice, rejoice, Emmanuel
-Shall come to thee, o Israel
+Shall come to thee, O Israel
  
 O come, Thou Day-Spring
 Come and cheer
@@ -19,19 +19,19 @@ Our spirits by Thine advent here
 Disperse the gloomy clouds of night
 And death’s dark shadows put to flight
 Rejoice, rejoice, Emmanuel
-Shall come to thee, o Israel
+Shall come to thee, O Israel
  
 O come, Thou Key of David, come
 And open wide our heavenly home
 Make safe the way that leads on high
 And close the path to misery
 Rejoice, rejoice, Emmanuel
-Shall come to thee, o Israel
+Shall come to thee, O Israel
  
 O come, O come, Thou Lord of might
 Who to Thy tribes, on Sinai’s height
 In ancient times did’st give the Law
 In cloud, and majesty and awe
 Rejoice, rejoice, Emmanuel
-Shall come to thee, o Israel
+Shall come to thee, O Israel
 `
